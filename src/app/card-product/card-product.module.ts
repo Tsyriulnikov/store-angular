8dc7f6086036329ccbe692877d10aa7ba56e5929
Tsyriulnikov <max_djvu@mail.ru>
2022-10-17 11:31:39 +0300

    main-page ver1

---
+++ src/app/card-product/card-product.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {CardProductComponent} from "./components/card-product/card-product.component";
+import {CardModule} from "primeng/card";
+import {ButtonModule} from "primeng/button";
+
+
+
+
+@NgModule({
+  declarations: [
+    CardProductComponent
+  ],
+  imports: [
+    CommonModule,
+    CardModule,
+    ButtonModule
+  ]
+})
+export class CardProductModule { }
