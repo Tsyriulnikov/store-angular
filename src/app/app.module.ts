754df7b5b0dc301953c25a9a21b8ee2cb4558671
Tsyriulnikov <max_djvu@mail.ru>
2022-10-18 15:30:19 +0300

    main-page ver3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HeaderModule} from "./header/header.module";
 import {MainPageModule} from "./main-page/main-page.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        HeaderModule,
-        MainPageModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderModule,
+    MainPageModule,
+    HttpClientModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
