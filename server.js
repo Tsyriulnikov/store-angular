db696bcbadc7a9368d7c38bd1b40eae0abb0f289
Tsyriulnikov <max_djvu@mail.ru>
2022-11-07 16:52:23 +0300

    deploy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/'));
+app.use(express.static(${__dirname}/dist/));
 
 app.get('/*', (req, res) => {
-  res.sendFile('index.html', { root: 'dist/' });
+  res.sendFile('./dist/index.html', { root: 'dist/' });
 });
 
 app.listen(process.env.PORT || 8080);
